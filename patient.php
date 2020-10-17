659a2fe3844be0fb101e17a1a78ba9dbcf3225f7
mohitz007 <mohitsmart2004@gmail.com>
2020-10-18 02:55:07 +0530

    Update patient.php

--- patient.php
+++ patient.php
@@ -11,6 +11,13 @@
 <body>
     <button onclick="getLocation()">Share your location</button>
         <div id="output"></div>
+        <div class="card a"><i class="fa fa-tachometer"></i><h5>Active Cases</h5><span id="active"></span></div>
+      <div class="card ca"><i class="fa fa-th-list"></i><h5>Total Cases</h5><span id="cases"></span></div>
+      <div class="card cr"><i class="fa fa-times-circle"></i><h5>Critical Cases</h5><span id="critical"></span></div>
+      <div class="card d"><i class="fa fa-times"></i><h5>Total Deaths</h5><span id="death"></span></div>
+      <div class="card r"><i class="fa fa-check-square-o"></i><h5>Recovered Cases</h5><span id="recovered"></span></div>
+      <div class="card t"><i class="fa fa-eye"></i><h5>Total Testes Done</h5><span id="tests"></span></div>
+    </div>
     <script>
         var x = document.getElementById('output');
         var longitude,latitude;
@@ -40,14 +47,16 @@
   )
   .then((data) => {
     console.log(data);
-    document.getElementById("country").innerHTML = data.country;
-    document.getElementById("active").innerHTML = data.active.toLocaleString();
-    document.getElementById("cases").innerHTML = data.cases.toLocaleString();
-    document.getElementById("critical").innerHTML = data.critical.toLocaleString();
-    document.getElementById("death").innerHTML = data.deaths.toLocaleString();
-    document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
-    document.getElementById("tests").innerHTML = data.tests.toLocaleString();
-    document.getElementById("flag").src = data.countryInfo.flag;
+    var json = data;
+    document.getElementById("output").innerHTML = data.result[0].name;
+    //  document.getElementById("country").innerHTML = data.country;
+     document.getElementById("active").innerHTML = data.result[1].name;
+    document.getElementById("cases").innerHTML = data.result[2].name;
+    document.getElementById("critical").innerHTML = data.result[3].name;
+    document.getElementById("death").innerHTML = data.result[4].name;
+    document.getElementById("recovered").innerHTML = data.result[5].name;
+    document.getElementById("tests").innerHTML = data.result[6].name;
+    // document.getElementById("flag").src = data.countryInfo.flag;
   });
         }
     // x.innerHTML = longitude + " " + " " + longitude;
